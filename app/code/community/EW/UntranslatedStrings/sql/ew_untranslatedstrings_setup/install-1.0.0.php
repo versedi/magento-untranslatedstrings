f11521f73c24c018fd945ae203307f0d4bcace58
versedi <versedi@gmail.com>
2017-02-03 09:31:20 +0100

    Change check if table exists into table factory

--- app/code/community/EW/UntranslatedStrings/sql/ew_untranslatedstrings_setup/install-1.0.0.php
+++ app/code/community/EW/UntranslatedStrings/sql/ew_untranslatedstrings_setup/install-1.0.0.php
@@ -5,37 +5,35 @@ $installer = $this;
 $installer->startSetup();
 
 $tableName = $installer->getTable('ew_untranslatedstrings/string');
-if(!Mage::getSingleton('core/resource')->getConnection('core_write')->isTableExists($tableName)) {
-    $table = new Varien_Db_Ddl_Table();
-    $table->setName($tableName);
+$table = $installer->getConnection()->newTable($tableName);
+$table->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, 11, array(
+    'nullable' => false, 'identity' => true, 'primary' => true,
+));
+$table->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_INTEGER, 11, array('nullable' => false));
+$table->addColumn('untranslated_string', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => false));
+$table->addColumn('translation_code', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => false));
+$table->addColumn('translation_module', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => true));
+$table->addColumn('locale', Varien_Db_Ddl_Table::TYPE_VARCHAR, 10, array('nullable' => true));
+$table->addColumn('url_found', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => true));
+$table->addColumn('date_found', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array('nullable' => false));
 
-    $table->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, 11, array('nullable' => FALSE, 'identity' => TRUE, 'primary' => TRUE));
-    $table->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_INTEGER, 11, array('nullable' => FALSE));
-    $table->addColumn('untranslated_string', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => FALSE));
-    $table->addColumn('translation_code', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => FALSE));
-    $table->addColumn('translation_module', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => TRUE));
-    $table->addColumn('locale', Varien_Db_Ddl_Table::TYPE_VARCHAR, 10, array('nullable' => TRUE));
-    $table->addColumn('url_found', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array('nullable' => TRUE));
-    $table->addColumn('date_found', Varien_Db_Ddl_Table::TYPE_DATETIME, NULL, array('nullable' => FALSE));
+$installer->getConnection()->createTable($table);
 
-    $installer->getConnection()->createTable($table);
-
-    $uniqueFields = array(
-        'store_id',
-        'untranslated_string',
-        'translation_code',
-        'locale'
-    );
-    $installer->getConnection()->addIndex(
+$uniqueFields = array(
+    'store_id',
+    'untranslated_string',
+    'translation_code',
+    'locale',
+);
+$installer->getConnection()->addIndex(
+    $tableName,
+    $installer->getIdxName(
         $tableName,
-        $installer->getIdxName(
-            $tableName,
-            $uniqueFields,
-            Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
-        ),
         $uniqueFields,
         Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
-    );
+    ),
+    $uniqueFields,
+    Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
+);
 
-}
 $installer->endSetup();
